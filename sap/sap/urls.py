adc6c4e113b4240d3d9e6ff4ace5e4f1a2c5d9f8
GLOOMYY <sebas.mesa.montoya@gmail.com>
2023-06-14 23:57:50 -0500

    MVT

--- sap/sap/urls.py
+++ sap/sap/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import bienvenido, despedirse, contacto
+from webapp.views import bienvenido, contacto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', bienvenido),
-    path('despedida/', despedirse),
     path('contacto/', contacto),
 ]
--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from personas.models import Persona
 
 # Create your views here.
 
 def bienvenido(request):
-  return render(request, 'prueba.html')
-
-def despedirse(request):
-  return HttpResponse('Chaito')
+  numPersonas = Persona.objects.count()
+  return render(request, 'prueba.html', {'numPersonas':numPersonas})
 
 def contacto(request):
   return HttpResponse("Nombre: Sebastian Mesa \nTelefono: 3053438319 \nEmail: sebas.mesa.montoya@gmail.com")
